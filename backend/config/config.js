ade12286f5e5419f8a87488f4587377fc95c94ac
Eivy <eivy@Eivys-MacBook-Air.local>
2025-10-16 19:28:32 -0600

    chore: add sequelize CLI JSON config

--- backend/config/config.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-const base = {
-  use_env_variable: 'DATABASE_URL',
-  dialect: 'postgres',
-  logging: false,
-  dialectOptions: {} // sin SSL para DB interna de Render
-};
-module.exports = { development: base, test: base, production: base };
